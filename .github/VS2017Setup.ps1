b5cda3926df1fc763fa699ac60f8b7dcd2bfce04
Laurens Van Houtven (lvh) <_@lvh.io>
2020-06-21 20:57:30 -0500

    More hail Maries

---
+++ .github/VS2017Setup.ps1
@@ -0,0 +1,4 @@
+choco install visualstudio2017community `
+  --version 15.9.17.0 `
+  --no-progress `
+  --package-parameters "--add Microsoft.VisualStudio.Component.VC.Tools.ARM64 --add Microsoft.VisualStudio.Component.VC.CMake.Project"
