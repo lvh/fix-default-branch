6ad42b41274489480d3af78b907e090a72e03dc9
Laurens Van Houtven (lvh) <_@lvh.io>
2020-06-27 16:36:47 -0500

    Lesson learned: don't put stuff in GitHub Action workflow files

--- windows/NativeImageWithCheese.cmd
+++ windows/NativeImageWithCheese.cmd
@@ -1,2 +1,14 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
-%JAVA_HOME%\bin\native-image %*
+%JAVA_HOME%\bin\native-image ^
+  -jar target\fix-default-branch.jar ^
+  --verbose ^
+  --no-fallback ^
+  --no-server ^
+  -H:Name=fix-default-branch ^
+  -H:+ReportExceptionStackTraces ^
+  --initialize-at-build-time ^
+  --report-unsupported-elements-at-runtime ^
+  --allow-incomplete-classpath ^
+  --enable-https ^
+  -J-Dclojure.spec.skip-macros=true ^
+  -J-Dclojure.compiler.direct-linking=true
